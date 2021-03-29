9dc37008a8c4df238c096082394fb11c80f3e8e1
Marta González <alu0101254487@ull.edu.es>
2021-03-29 16:13:10 +0000

    One branch executed

--- examples/test/coverage-example.spec.js
+++ examples/test/coverage-example.spec.js
@@ -5,11 +5,11 @@ describe('Useless function', () => {
   it('Function exists', () => {
     expect(uselessFunction()).to.exist;
   });
-  /*
+
   it('Only x value is greater than 0', () => {
     expect(uselessFunction(1, -1)).to.equal(0);
   });
-
+/*
   it('Both values are greater than 0', () => {
     expect(uselessFunction(1, 1)).to.equal(1);
   });*/
