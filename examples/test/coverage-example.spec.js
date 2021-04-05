447eecb158a8c9d4c70b10442ec84be0cd14b920
Marta González <alu0101254487@ull.edu.es>
2021-04-05 09:07:47 +0000

    uselessFunction exists

--- examples/test/coverage-example.spec.js
+++ examples/test/coverage-example.spec.js
@@ -2,11 +2,11 @@ const {expect} = require('chai');
 const {uselessFunction} = require('../src/coverage-example.js');
 
 describe('Useless function', () => {
-  /*it('Function exists', () => {
+  it('Function exists', () => {
     expect(uselessFunction()).to.exist;
   });
 
-  it('Only x value is greater than 0', () => {
+  /*it('Only x value is greater than 0', () => {
     expect(uselessFunction(1, -1)).to.equal(0);
   });
 
